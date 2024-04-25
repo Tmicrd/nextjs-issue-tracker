f92347db24cb98bc74fdbb6458ac334aeebafba5
nancy <liyingxiong23@gmail.com>
2024-04-25 23:08:50 +1000

    Customise Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "@radix-ui/themes/styles.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "@/app/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,9 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme>
+        <Theme accentColor="violet" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
+          {/* <ThemePanel /> use this ThemePanel to customise theme  */}
         </Theme>
       </body>
     </html>
