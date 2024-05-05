49896d2c31a195a223ebcac74f862b32d17b0012
nancy <liyingxiong23@gmail.com>
2024-05-05 16:02:55 +1000

    Add delete issue button

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -1,9 +1,9 @@
-import { IssueStatusBadge } from '@/app/components';
-import { Issue } from '@prisma/client';
-import { Box, Card, Flex, Heading, Text } from '@radix-ui/themes';
-import ReactMarkdown from 'react-markdown';
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
 
-const IssueDetails = ({ issue }:{ issue: Issue }) => {
+const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
     <>
       <Box>
@@ -12,12 +12,12 @@ const IssueDetails = ({ issue }:{ issue: Issue }) => {
           <IssueStatusBadge status={issue.status} />
           <Text>{issue.createdAt.toDateString()}</Text>
         </Flex>
-        <Card className="prose" mt="4">
+        <Card className="prose max-w-full" mt="4">
           <ReactMarkdown>{issue.description}</ReactMarkdown>
         </Card>
       </Box>
     </>
   );
-}
+};
 
-export default IssueDetails
+export default IssueDetails;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
+import NavBar from "@/app/NavBar";
+import { Container, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme, ThemePanel } from "@radix-ui/themes";
-import NavBar from "@/app/NavBar";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +22,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme accentColor="violet" radius="large">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
           {/* <ThemePanel /> use this ThemePanel to customise theme  */}
         </Theme>
       </body>
