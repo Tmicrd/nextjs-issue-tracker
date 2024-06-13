820fc38f3411ce5de1a5bd1f4679e0fb77ecf2aa
nancy <liyingxiong23@gmail.com>
2024-06-13 10:52:06 +1000

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -10,7 +10,6 @@ interface Props {
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  console.log('searchParams: ', searchParams);
   const columns: { label: string; value: keyof Issue; className?: string }[] = [
     { label: "Issue", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
@@ -21,13 +20,20 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? {
+        [searchParams.orderBy]: "asc",
+      }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
-    orderBy: {
-      createdAt: "desc",
-    },
+    orderBy,
   });
 
   return (
